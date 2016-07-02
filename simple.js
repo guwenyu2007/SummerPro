ad958e42c1f6cc699216d14cf7b0def90ca8fc0f
Gust <573848754@qq.com>
2016-07-02 16:08:38 +0800

    text

---
+++ simple.js
@@ -0,0 +1,9 @@
+
+function  clausum() {
+    var value1, value2, sum;
+    value1 = parseInt(document.mainForm.textBox1.value);
+    value2 = parseInt(document.mainFrom.textBox2.value);
+    
+    sum = value1 + value2;
+    alert(sum);
+}
